5da4ac04308c5f581a44ee5e6d03bdc602c42e63
Threetattoo <mikhailpazyuk@yandex.ru>
2021-09-03 23:00:12 +0500

    add fixes: sprint 11 iteration two

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import PopupWithForm from './PopupWithForm';
 
-function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
+function EditAvatarPopup({isOpen, onClose, onUpdateAvatar}) {
+  const avatarRef = React.useRef (null);
 
-    const avatarRef = React.useRef(null);
-  
-    function handleSubmit(event) {
-        event.preventDefault();
-        onUpdateAvatar({ avatar: avatarRef.current.value });
-        event.currentTarget.reset();
-    }
-  
-    return (
-        <PopupWithForm name="edit-avatar" title="Обновить аватар" buttonText="Сохранить" isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit}>
-            <label className="popup__form-label">
-                <input ref={avatarRef} type="url" required className="popup__input" placeholder="Ссылка на картинку" name="avatarLink" id="avatar-link" />
-                <span className="popup__error avatar-link-error popup__error-visible"></span>
-            </label>
-        </PopupWithForm>
-    )
+  function handleSubmit (event) {
+    event.preventDefault ();
+    onUpdateAvatar ({avatar: avatarRef.current.value});
   }
-  
-  export default EditAvatarPopup;
+
+  React.useEffect(() => {
+    avatarRef.current.value = '';
+  },
+    [isOpen]
+  )
+
+  return (
+    <PopupWithForm
+      name="edit-avatar"
+      title="Обновить аватар"
+      buttonText="Сохранить"
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleSubmit}
+    >
+      <label className="popup__form-label">
+        <input
+          ref={avatarRef}
+          type="url"
+          required
+          className="popup__input"
+          placeholder="Ссылка на картинку"
+          name="avatarLink"
+          id="avatar-link"
+        />
+        <span className="popup__error avatar-link-error popup__error-visible" />
+      </label>
+    </PopupWithForm>
+  );
+}
+
+export default EditAvatarPopup;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-function Footer() {
-    return(
-        <footer className="footer">
-            <p className="footer__copywright">
-                &copy; 2020 Mesto Russia
-            </p>
-        </footer>
-    );
+function Footer () {
+  return (
+    <footer className="footer">
+      <p className="footer__copywright">
+        &copy; 2020 Mesto Russia
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
